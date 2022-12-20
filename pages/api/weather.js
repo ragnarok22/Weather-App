e1421211653768f70a0321878ff5cf78fad0f9c0
Reinier Hernández <sasuke.reinier@gmail.com>
2022-12-20 16:44:22 -0500

    feat: get the weather info of one city

---
+++ pages/api/weather.js
@@ -0,0 +1,53 @@
+// get the weather of a city
+import axios from "axios"
+
+
+function addDays(date, number) {
+  const newDate = new Date(date);
+  return new Date(newDate.setDate(date.getDate() + number));
+}
+
+function parseDate(date) {
+  return date.toISOString().split('T')[0]
+}
+
+export default async function handler(req, res) {
+  const { latitude, longitude } = req.query
+  let { temperature_unit } = req.query
+  temperature_unit = temperature_unit || "celsius"
+
+  if (latitude && longitude && (temperature_unit === "celsius" || temperature_unit === "fahrenheit")) {
+    try{
+      const response = await axios.get("https://api.open-meteo.com/v1/forecast", {
+        params: {
+          latitude,
+          longitude,
+          temperature_unit,
+          daily: "temperature_2m_max,temperature_2m_min",
+          current_weather: true,
+          timezone: "America/Los_Angeles",
+          start_date: parseDate(new Date()),
+          end_date: parseDate(addDays(new Date(), 4)) // get the max and the min of the next four days
+        }
+      })
+
+      const results = {
+        latitude,
+        longitude,
+        weathercode: response.data.current_weather.weathercode,
+        temperatures: response.data.daily.temperature_2m_max.map((item, index) => {
+          // get the average temperature of the day + the next days
+          return parseInt((item + response.data.daily.temperature_2m_min[index]) / 2)
+        })
+      }
+
+      res.status(200).json(results)
+    } catch (e) {
+      res.status(500).json(e)
+    }
+  } else {
+    res.status(400).json({
+      message: "You should provide latitude and longitude"
+    })
+  }
+}
